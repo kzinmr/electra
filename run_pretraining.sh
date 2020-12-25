4dfcbdc1311ccc74741b47fcd692723232c5f749
Kazuki Inamura <kzinmr109@gmail.com>
2020-12-25 22:40:21 +0900

    add: merge tfrecords & pretraining

--- run_pretraining.sh
+++ run_pretraining.sh
@@ -1,6 +1,8 @@
-CORPUS_DIR=/app/outputs/data/
-VOCAB_FILE=/app/outputs/vocab.txt
-OUTPUT_DIR=/app/outputs/pretrain_tfrecords
+DATA_DIR=/app/outputs/
+
+CORPUS_DIR=${DATA_DIR}/data/
+VOCAB_FILE=${DATA_DIR}/vocab.txt
+OUTPUT_DIR=${DATA_DIR}/pretrain_tfrecords
 MAX_SEQ_LENTGH=128
 NUM_PROCESSES=4
 
@@ -13,3 +15,8 @@ python3 build_pretraining_dataset_tfrecords.py \
 --output-dir=$OUTPUT_DIR \
 --max-seq-length=$MAX_SEQ_LENTGH \
 --num-processes=$NUM_PROCESSES
+
+
+MODEL_NAME=electra_small_wiki40b_ja_mecab_ipadic
+python3 run_pretraining.py --data-dir $DATA_DIR --model-name $MODEL_NAME
+# --hparams setting.json
