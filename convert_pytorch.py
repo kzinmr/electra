7b9935f91de74aca4b044b16bebc7fe2fe0b8d81
Kazuki Inamura <kzinmr109@gmail.com>
2020-12-28 17:18:58 +0900

    fix

--- convert_pytorch.py
+++ convert_pytorch.py
@@ -1,18 +1,37 @@
 import os
 # 1. export config from config class
 from configure_pretraining import PretrainingConfig
-from util import training_utils
+# from util import training_utils
+from transformers import BertConfig, BertModel
+
+def get_bert_config(config):
+    """Get model hyperparameters based on a pretraining/finetuning config"""
+    if config.model_size == "large":
+        args = {"hidden_size": 1024, "num_hidden_layers": 24}
+    elif config.model_size == "base":
+        args = {"hidden_size": 768, "num_hidden_layers": 12}
+    elif config.model_size == "small":
+        args = {"hidden_size": 256, "num_hidden_layers": 12}
+    else:
+        raise ValueError("Unknown model size", config.model_size)
+    args["vocab_size"] = config.vocab_size
+    args.update(**config.model_hparam_overrides)
+    # by default the ff size and num attn heads are determined by the hidden size
+    args["num_attention_heads"] = max(1, args["hidden_size"] // 64)
+    args["intermediate_size"] = 4 * args["hidden_size"]
+    args.update(**config.model_hparam_overrides)
+    return BertConfig.from_dict(args)  # use transformers instead
+
 
 model_name = 'electra_small_wiki40b_ja_mecab_ipadic'
 data_dir = '/app/outputs/models'
 config = PretrainingConfig(model_name, data_dir)
-bert_config = training_utils.get_bert_config(config)
+bert_config = get_bert_config(config)
 config_file =f'{data_dir}/bert_config.json'
 with open(config_file, "w") as f:
   f.write(bert_config.to_json_string())
 
 # 2. export pytorch model
-from transformers import BertConfig, BertModel
 
 ckpt_index_file = f'{data_dir}/model.ckpt-xxxx.index'
 config = BertConfig.from_json_file(config_file)
