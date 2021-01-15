200c189ec32826e8420928deef390714b6387701
Kazuki Inamura <kzinmr109@gmail.com>
2021-01-15 15:56:44 +0900

    change vocab setting

--- build_pretraining_dataset_preprocess.py
+++ build_pretraining_dataset_preprocess.py
@@ -1,18 +1,4 @@
 # coding=utf-8
-# Copyright 2020 The Google Research Authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 """Writes out text data as tfrecords that ELECTRA can be pre-trained on."""
 
 import argparse
@@ -169,8 +155,8 @@ def main():
 
     settings = dict(
         vocab_size=30000,
-        min_frequency=2,
-        limit_alphabet=1000,
+        min_frequency=5,
+        limit_alphabet=3000,
     )
     tokenization_hf.train_custom_tokenizer(fnames, args.tokenizer_file, **settings)
 
